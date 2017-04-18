529baf2d02a1af0e24a28d3040b97ba2000d5a74
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-04-18 17:35:07 +0100

    server side redux controller namespace update

--- server/store/reducer.js
+++ server/store/reducer.js
@@ -1,14 +1,15 @@
 import {fromJS} from 'immutable';
 import {restart, next, guess} from './core';
 import storedState from 'server/state.json';
+import {RESTART, NEXT, GUESS} from 'client/controllers/hangman';
 
 export default function reducer(state=fromJS(storedState), action){
 	switch (action.type) {
-	case 'RESTART':
+	case RESTART:
 		return next(restart(state));
-	case 'NEXT':
+	case NEXT:
 		return next(state);
-	case 'GUESS':
+	case GUESS:
 		return guess(state, action.letter);
 	default:
 		return state;
