f4fc774a68264206b34ae494142246abac844f13
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-06-09 17:18:15 +0100

    eslint WIP: lock eslint-config-airbnb peer dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   extends: 'airbnb',
+  parser: 'babel-eslint',
   env: {
     browser: true,
     node: true,
     es6: true,
     'jest/globals': true
   },
-  parser: 'babel-eslint',
   "parserOptions": {
     "ecmaVersion": 6,
     "sourceType": "module",
@@ -14,15 +14,20 @@ module.exports = {
       "jsx": true
     }
   },
-  plugins: ['jest'],
+  plugins: [
+    'jest',
+  ],
   rules: {
     "react/react-in-jsx-scope": "off",
     "import/no-extraneous-dependencies": "off",
     "import/no-named-as-default": "off",
+    "import/extensions": [1, {
+      "js": "never"
+    }],
     "prefer-default-export": "off",
     "react/jsx-indent-props": [2, 2],
     "react/require-default-props": "off",
-  	'jest/no-disabled-tests': 'warn',
+    'jest/no-disabled-tests': 'warn',
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
     'jest/valid-expect': 'error',
