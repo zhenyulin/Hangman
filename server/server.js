cabc23dcc05b335c4c971507d8bd6f409645035c
Chris Blossom <cbloss@gmail.com>
2017-06-12 10:54:16 -0600

    remove unsupported babel-plugin-add-module-exports as it was causing issues with the client and only used for one line in server.js

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ import connectDB from 'server/config/db';
 import configIO from 'server/socket';
 import router from 'server/router';
 import { PORT } from 'server/config/constant';
+import helmet from 'helmet';
 
 const SERVER_START = `server started on port ${PORT}`;
 console.time(SERVER_START);
@@ -28,17 +29,17 @@ if (process.env.NODE_ENV !== 'production') {
   const webpack = require('webpack');
   const webpackDevMiddleware = require('webpack-dev-middleware-webpack-2');
   const webpackHotMiddleware = require('webpack-hot-middleware');
-  const webpackConfig = require('config/webpack.dev');
+  const webpackConfig = require('config/webpack.dev').default;
   const compiler = webpack(webpackConfig);
   app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: webpackConfig.output.publicPath,
   }));
   app.use(webpackHotMiddleware(compiler));
+
   const morgan = require('morgan');
   app.use(morgan('dev'));
 } else {
-  const helmet = require('helmet');
   app.use(helmet());
 }
 
