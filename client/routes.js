dc345494c939649633957d0d4f7f9dbef3f5091f
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-06-14 22:25:44 +0100

    Revert "add router test views [stub]"
    This reverts commit 574b800c99800a9f344b7000cb28611f4e8150c3.

--- client/routes.js
+++ client/routes.js
@@ -3,14 +3,10 @@ import { Route } from 'react-router';
 
 import Hangman from './containers/hangman';
 import Management from './containers/management';
-import HelloWorld from './containers/hello-world';
-import Test from './containers/test';
 
 const routes = (
   <Route>
-    <Route path="/" component={HelloWorld} />
-    <Route path="/test" component={Test} />
-    <Route path="/hangman" component={Hangman} />
+    <Route path="/" component={Hangman} />
     <Route path="/management" component={Management} />
   </Route>
 );
