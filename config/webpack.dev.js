43ba37c842c19ed50c91712f41686a9ee9c61d7c
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-06-14 22:23:00 +0100

    fix react-hot-loader
    several problem here:
    - disable modules will disable the “import/export” on the backend as
    well, which can cause collisions and make packages not working, so the
    solution is leave it there, but using require module.default on frontend
    - preset-flow will strip out some code, which makes hot server
    middleware not working

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -5,11 +5,11 @@ import HTMLWebpackPlugin from 'html-webpack-plugin';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-export default {
-  devtool: 'inline-source-map',
+module.exports = {
+  devtool: 'cheap-module-source-map',
   entry: [
-    'react-hot-loader/patch',
     'webpack-hot-middleware/client',
+    'react-hot-loader/patch',
     path.resolve('./client/index.js'),
   ],
   output: {
