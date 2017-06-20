cf1e24b99f54e141d5e362f963ae1aca652dbc03
Zhenyu Lin <zhenyu.lyn@gmail.com>
2017-06-20 21:40:52 +0100

    remove excessive server side index page handler

--- server/router.js
+++ server/router.js
@@ -1,11 +1,9 @@
 import express from 'express';
 
-import index from 'server/handlers/index';
 import auth from 'server/handlers/auth';
 
 const router = express.Router();
 
 router.use('/auth', auth);
-router.use('/', index);
 
 export default router;
